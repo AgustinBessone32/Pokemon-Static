cb66b3ae39f48c5e2d75e0f2b9ef467531ddd6c5
Agustin Bessone <bessone98@gmail.com>
2022-03-15 20:24:04 -0300

    se agrega key

--- components/pokemon/FavoritesPokemons.tsx
+++ components/pokemon/FavoritesPokemons.tsx
@@ -12,7 +12,7 @@ export const FavoritesPokemons: FC<Props> = ({pokemons}) => {
         <Grid.Container gap={2} direction='row' justify='flex-start'>
             {
                 pokemons.map(id => (
-                    <FavoriteCardPokemon pokemonId={id} />
+                    <FavoriteCardPokemon pokemonId={id} key={id} />
                 ))
             }
         </Grid.Container>

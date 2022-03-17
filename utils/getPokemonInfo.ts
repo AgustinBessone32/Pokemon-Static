c943b951e43e31926ac5037dd2bde435d4b357d4
Agustin Bessone <bessone98@gmail.com>
2022-03-16 21:03:59 -0300

    agrego incremental static regeneration

--- utils/getPokemonInfo.ts
+++ utils/getPokemonInfo.ts
@@ -4,15 +4,19 @@ import { Pokemon } from '../interfaces';
 
 export const getPokemonInfo = async(nameOrId: string) => {
 
+    try{
+        const { data } = await pokeApi.get<Pokemon>(`/pokemon/${nameOrId}`)
 
-    const { data } = await pokeApi.get<Pokemon>(`/pokemon/${nameOrId}`)
+        return{
+            id: data.id,
+            name: data.name,
+            sprites: data.sprites
+        }
 
-    const pokemon = {
-        id: data.id,
-        name: data.name,
-        sprites: data.sprites
+    }catch(err) {
+        return null
     }
 
-    return pokemon
+
 
 }
